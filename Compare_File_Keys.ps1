600345292dd6e04a0abd31e38cf017b62904836f
Reebock071991 <68036786+Reebock071991@users.noreply.github.com>
2020-07-10 15:07:47 -0600

    Add files via upload

---
+++ Compare_File_Keys.ps1
@@ -0,0 +1,9 @@
+﻿
+##
+The below script compare the sha 256 keys of a file downloaded and the key on the web
+##
+$wc = [System.Net.WebClient]::new()
+$pkgurl = ' C:\Users\Reebo\Downloads\VirtualBox-6.1.10-138449-Win.exe '
+$publishedHash = '5bc643d09a15bb105f1df37f14e2024f88e82019921ead3cd9a43bee7290c762'
+$FileHash = Get-FileHash -Algorith SHA256 -InputStream ($wc.OpenRead($pkgurl))
+$FileHash.Hash.ToLower() -eq $publishedHash
